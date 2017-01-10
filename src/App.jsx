4e094bc6d0b71b877d7b17df879b28f153a300cb
Tomas Ohlson <ohlson.tomas@gmail.com>
2017-01-10 22:39:30 +0100

    some more dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 export default class App extends Component {
     render() {
         return (
-            <h1>Hello, world ddd</h1>
+            <MuiThemeProvider>
+                <h1>Hello, world ddd</h1>
+            </MuiThemeProvider>
         )
     }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -21,5 +21,8 @@ module.exports = {
             loaders: ['react-hot', 'babel'],
             include: path.join(__dirname, 'src')
         }]
-    }
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx', '.css'],
+    },
 }
