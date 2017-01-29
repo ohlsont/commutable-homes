00ad3d1686d5a9f276ea72977f9cf4f3bec60670
Tomas Ohlson <ohlson.tomas@gmail.com>
2017-01-29 15:12:48 +0100

    add stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,8 @@
 import React, { Component } from 'react'
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import TwitterManager from './twitter'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 
 export default class App extends Component {
-    async getTwitterStatus() {
-        const resp = await TwitterManager.getStatuses()
-        console.log('debug ', resp)
-    }
-
     render() {
-        this.getTwitterStatus()
         return (
             <MuiThemeProvider>
                 <h1>Hello, world ddd</h1>
